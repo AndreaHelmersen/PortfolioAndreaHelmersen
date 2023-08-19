62947c295b88909c383c459db07b66f7c1505b74
Andrea Arstad Helmersen <andrahe@ulrik.uio.no>
2023-08-19 22:20:20 +0200

    Changed projectcard logic for image and extra text

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,9 +4,9 @@ interface ProjectCardProps {
   title: string;
   projectText: string;
   knowledgeText: string;
-  image: string;
   link: string;
   workedOn: string;
+  image?: string;
   ideaText?: string;
   companyText?: string;
 }
@@ -24,7 +24,7 @@ export const ProjectCard = (props: ProjectCardProps) => {
   return (
     <div style={{ display: "flex", padding: "50px" }}>
       <div style={{ width: "70%" }}>
-      <div style={{ display: "flex", alignItems: "baseline" }}>
+        <div style={{ display: "flex", alignItems: "baseline" }}>
           <div style={{ flex: 1 }}>
             <Typography variant="h3" style={corbelFontStyle}>
               {props.title}
@@ -35,7 +35,7 @@ export const ProjectCard = (props: ProjectCardProps) => {
           </Typography>
         </div>
 
-        <hr style={{ width: "100%", border: "1px solid #ccc" }} /> 
+        <hr style={{ width: "100%", border: "1px solid #ccc" }} />
 
         <div style={corbelFontStyle}>
           <Typography sx={{ pt: "10px", fontWeight: "800" }}>
@@ -43,12 +43,23 @@ export const ProjectCard = (props: ProjectCardProps) => {
           </Typography>
           <Typography>{props.projectText}</Typography>
 
-          <Typography sx={{ pt: "15px", fontWeight: "800" }}>
-            {props.ideaText ? "The Idea" : "The Company"}
-          </Typography>
-          <Typography>
-            {props.ideaText ? props.ideaText : props.companyText}
-          </Typography>
+          {props.ideaText && (
+            <>
+              <Typography sx={{ pt: "15px", fontWeight: "800" }}>
+                The Idea
+              </Typography>
+              <Typography>{props.ideaText}</Typography>
+            </>
+          )}
+
+          {props.companyText && (
+            <>
+              <Typography sx={{ pt: "15px", fontWeight: "800" }}>
+                The Company
+              </Typography>
+              <Typography>{props.companyText}</Typography>
+            </>
+          )}
 
           <Typography sx={{ pt: "16px", fontWeight: "800" }}>
             Acquired Knowledge
@@ -63,13 +74,15 @@ export const ProjectCard = (props: ProjectCardProps) => {
         </div>
       </div>
 
-      <div style={{ width: "30%", margin: "20px", overflow: "hidden" }}>
-        <img
-          src={props.image}
-          alt="Project Image"
-          style={{ width: "100%", height: "auto" }}
-        />
-      </div>
+      {props.image && (
+        <div style={{ width: "30%", margin: "20px", overflow: "hidden" }}>
+          <img
+            src={props.image}
+            alt="Project Image"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </div>
+      )}
     </div>
   );
 };
