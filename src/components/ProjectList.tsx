808ce40c398f70d8abbe414efba119f069c32637
Andrea Arstad Helmersen <andrahe@ulrik.uio.no>
2023-08-19 22:34:20 +0200

    Moved the constant porject data to own file

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,68 +1,10 @@
 import { ProjectCard } from "./ProjectCard";
-import deconXImage from "../Images/DeconX.png";
-
-const projects = [
-  {
-    title: "Decon-X",
-    projectText:
-      "The project involved conducting various strategic analyses based on interviews conducted with a selected company, as well as identifying a strategic challenge. Based on the challenge, we developed three marketing strategies to 'address' the challenge and created an action plan for the best solution.",
-    companyText:
-      "The company we worked with for this project was Decon-X. They provide an automated disinfection robot.",
-    knowledgeText:
-      "Strategic analysis (PESTEL, Five Forces, VRIO, SWOT, etc.), developing strategic solutions, strategic planning, marketing strategies, interview techniques, teamwork.",
-    link: "https://gitfront.io/r/AndreaHelmersen/Eo62QB3V989J/Decon-X/",
-    image: deconXImage,
-    workedOn: "Spring 2022",
-  },
-  {
-    title: "Bewere",
-    projectText:
-      "The project involved creating an Android app in Android Studio that utilized information from several of the Meteorological Institute's APIs. A part of the project was to document the work and processes well in a report. The project was carried out in a team of six students.",
-    IdeaText:
-      "Idea: The app provides an overview of various everyday risk factors that may give cause for concern. The user selects a preferred location on the map in the app, and the hazards are then displayed.",
-    knowledgeText:
-      "Acquired Knowledge: Team software development, teamwork, agile practices, documentation of development elements (modeling, requirements specifications, evaluation, and product, test, user, and process documentation), Android Studio, Kotlin, Xml, Json, Git, Jira.",
-    link: "https://gitfront.io/r/AndreaHelmersen/MsD28eJ9hebs/Bewere/",
-    //image: bewereImage, // Provide the image URL for Bewere
-    workedOn: "Spring 2022",
-  },
-  {
-    title: "ASP Interpreter",
-    projectText:
-      "The project involved creating an ASP interpreter in Java. The ASP programming language is a simplified version of Python. The project was divided into modules: scanner, parser, expression evaluation, and statement and function evaluation, which together built an interpreter for ASP. Projects were carried out in pairs of two.",
-    knowledgeText:
-      "Acquired Knowledge: Java, pair programming, collaboration, object-oriented programming in larger projects, use of modules in larger projects, debugging, interpreting and understanding code written by others.",
-    link: "https://gitfront.io/r/AndreaHelmersen/18ef300e5e098d17360269ff8f91dba72998652a/Asp-Interpreter/",
-    //image: aspInterpreterImage, // Provide the image URL for the ASP Interpreter project
-    workedOn: "Autumn 2021",
-  },
-  {
-    title: "Digital Transformation of Norsk Gjenvinning (Norwegian Recycling)",
-    projectText:
-      "The project focused on formulating measures for a digital transformation of Norsk Gjenvinning. In this project, we worked on formulating changes in NG to achieve a desired future. In this context, resistance to change is highly relevant, which I found to be one of the most interesting aspects of the task.",
-    knowledgeText:
-      "Acquired Knowledge: Digital transformation, change management and resistance, assessment of organizational culture and environments, formulation of measures to achieve a desired future (measures for structure, leadership, and change strategy), teamwork.",
-    link: "https://gitfront.io/r/AndreaHelmersen/d3d9f53f86a99d61ab5f529f4298894952e66a2b/Digital-transformasjon-av-NG/",
-    //image: ngDigitalTransformationImage, // Provide the image URL for the NG Digital Transformation project
-    workedOn: "Autumn 2021",
-  },
-  {
-    title: "Project Work Tise",
-    projectText:
-      "In this project, we started with a product from a Norwegian high-tech company, mainly developing a plan for the product, distribution, pricing, and marketing communication for the selected target audience in 2022. The chosen product was Tise's sharing economy platform, primarily for the used clothing market.",
-
-    knowledgeText:
-      "Acquired Knowledge: Analysis of microenvironments, assessment of competitive advantages ('Sweet Spot'), market segmentation, market research, marketing plan development, teamwork.",
-    link: "gitfront.io/r/AndreaHelmersen/1c2a35fad9c12503c16a010c281db885c3387e9c/Prosjektoppgave-Tise/",
-    //image: tiseSpring2021Image, // Provide the image URL for the Tise Spring 2021 project
-    workedOn: "Spring 2021",
-  },
-];
+import { STATIC_PROJECT_DATA } from "../staticData/ProjectData";
 
 export const ProjectList = () => {
   return (
     <div>
-      {projects.map((project, index) => (
+      {STATIC_PROJECT_DATA.map((project, index) => (
         <ProjectCard key={index} {...project} />
       ))}
     </div>
