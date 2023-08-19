1344b81eed9ca09953b2d190bf7354bcfa9f6eba
Andrea Arstad Helmersen <andrahe@ulrik.uio.no>
2023-08-20 00:17:21 +0200

    Styled projectCard box

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -22,73 +22,85 @@ export const ProjectCard = (props: ProjectCardProps) => {
   };
 
   return (
-    <div style={{ display: "flex", padding: "50px" }}>
-      <div style={{ width: "70%" }}>
-        <div style={{ display: "flex", alignItems: "baseline" }}>
-          <div style={{ flex: 1 }}>
-            <Typography variant="h3" style={corbelFontStyle}>
-              {props.title}
+    <div style={{ padding: "50px" }}>
+      <div
+        style={{
+          display: "flex",
+          padding: "50px",
+          minHeight: "85vh",
+          backgroundColor: "#fae6c5",
+          transformOrigin: "top center",
+          borderRadius: "20px",
+          boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
+        }}
+      >
+        <div style={{ width: "70%" }}>
+          <div style={{ display: "flex", alignItems: "baseline" }}>
+            <div style={{ flex: 1 }}>
+              <Typography variant="h3" style={corbelFontStyle}>
+                {props.title}
+              </Typography>
+            </div>
+            <Typography variant="h6" style={corbelFontStyle}>
+              {props.workedOn}
             </Typography>
           </div>
-          <Typography variant="h6" style={corbelFontStyle}>
-            {props.workedOn}
-          </Typography>
+
+          <hr style={{ width: "100%", border: "1px solid #ccc" ,borderColor: "black",}} />
+
+          <div style={corbelFontStyle}>
+            <Typography sx={{ pt: "10px", fontWeight: "800" }}>
+              The Project
+            </Typography>
+            <Typography>{props.projectText}</Typography>
+
+            {props.ideaText && (
+              <>
+                <Typography sx={{ pt: "15px", fontWeight: "800" }}>
+                  The Idea
+                </Typography>
+                <Typography>{props.ideaText}</Typography>
+              </>
+            )}
+
+            {props.companyText && (
+              <>
+                <Typography sx={{ pt: "15px", fontWeight: "800" }}>
+                  The Company
+                </Typography>
+                <Typography>{props.companyText}</Typography>
+              </>
+            )}
+
+            <Typography sx={{ pt: "16px", fontWeight: "800" }}>
+              Acquired Knowledge
+            </Typography>
+            <Typography>{props.knowledgeText}</Typography>
+
+            <Typography style={corbelFontStyle} sx={{ pt: "10px" }}>
+              <a href={props.link} style={linkStyle}>
+                Link to project
+              </a>
+            </Typography>
+          </div>
         </div>
 
-        <hr style={{ width: "100%", border: "1px solid #ccc" }} />
-
-        <div style={corbelFontStyle}>
-          <Typography sx={{ pt: "10px", fontWeight: "800" }}>
-            The Project
-          </Typography>
-          <Typography>{props.projectText}</Typography>
-
-          {props.ideaText && (
-            <>
-              <Typography sx={{ pt: "15px", fontWeight: "800" }}>
-                The Idea
-              </Typography>
-              <Typography>{props.ideaText}</Typography>
-            </>
-          )}
-
-          {props.companyText && (
-            <>
-              <Typography sx={{ pt: "15px", fontWeight: "800" }}>
-                The Company
-              </Typography>
-              <Typography>{props.companyText}</Typography>
-            </>
-          )}
-
-          <Typography sx={{ pt: "16px", fontWeight: "800" }}>
-            Acquired Knowledge
-          </Typography>
-          <Typography>{props.knowledgeText}</Typography>
-
-          <Typography style={corbelFontStyle} sx={{ pt: "10px" }}>
-            <a href={props.link} style={linkStyle}>
-              Link to project
-            </a>
-          </Typography>
-        </div>
+        {props.image && (
+          <div style={{ width: "600px", margin: "20px" }}>
+            <img
+              src={props.image}
+              alt="Project Image"
+              style={{
+                maxWidth: "100%",
+                height: "auto",
+                maxHeight: "400px",
+                display: "block",
+                margin: "0 auto",
+              }}
+            />
+          </div>
+        )}
       </div>
-
-      {props.image && (
-        <div style={{ width: "600px", margin: "20px" }}>
-          <img
-            src={props.image}
-            alt="Project Image"
-            style={{
-              maxWidth: "100%",
-              height: "auto",
-              maxHeight: "400px",
-              display: "block",
-              margin: "0 auto",
-            }}
-          />
-        </div>
-      )}
     </div>
   );
 };
