4b0c3b18c4894b775e1f6b67b8c9c8702cf0bf1e
Andrea Arstad Helmersen <andrahe@ulrik.uio.no>
2023-08-22 13:05:16 +0200

    New feature added formcontrol to choose between business or tech projects

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,26 +1,66 @@
 import { ProjectCard } from "./ProjectCard";
-import { STATIC_PROJECT_DATA } from "../staticData/ProjectData";
-import { Typography } from "@mui/material";
+import {
+  STATIC_PROJECT_DATA_bus,
+  STATIC_PROJECT_DATA_tech,
+} from "../staticData/ProjectData";
+import {
+  FormControl,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 
 export const ProjectList = () => {
   const corbelFontStyle = {
     fontFamily: "Corbel, Arial, sans-serif",
   };
+  const [option, setOption] = useState("TechProjects");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setOption(event.target.value);
+  };
+
+  const selectedProjectData =
+    option === "TechProjects"
+      ? STATIC_PROJECT_DATA_tech
+      : STATIC_PROJECT_DATA_bus;
+
   return (
-    <div style={{paddingTop: "150px"}}>
-      <div style={{ textAlign: "center" }}>
+    <>
+      <div
+        style={{
+          paddingTop: "150px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
         <Typography
           variant="h4"
           style={corbelFontStyle}
-          sx={{ fontWeight: "700", pb: "20px"}}
+          sx={{ fontWeight: "700" }}
         >
           PROJECTS
         </Typography>
-      </div>
 
-      {STATIC_PROJECT_DATA.map((project, index) => (
-        <ProjectCard key={index} {...project} />
-      ))}
-    </div>
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <Select
+            value={option}
+            onChange={handleChange}
+            displayEmpty
+            inputProps={{ "aria-label": "Without label" }}
+          >
+            <MenuItem value={"TechProjects"}>Technical Projects</MenuItem>
+            <MenuItem value={"busProjects"}>Business Projects</MenuItem>
+          </Select>
+        </FormControl>
+
+        {selectedProjectData.map((project, index) => (
+          <ProjectCard key={index} {...project} />
+        ))}
+      </div>
+    </>
   );
 };
--- src/staticData/ProjectData.tsx
+++ src/staticData/ProjectData.tsx
@@ -4,8 +4,32 @@ import Bewere from "../Images/Bewere.png";
 import Tise from "../Images/Tise.png";
 import NG from "../Images/NG.png";
 
+export const STATIC_PROJECT_DATA_tech = [
+  {
+    title: "Bewere",
+    projectText:
+      "The project involved creating an Android app in Android Studio that utilized information from several of the Meteorological Institute's APIs. A part of the project was to document the work and processes well in a report. The project was carried out in a team of six students.",
+    IdeaText:
+      "Idea: The app provides an overview of various everyday risk factors that may give cause for concern. The user selects a preferred location on the map in the app, and the hazards are then displayed.",
+    knowledgeText:
+      "Acquired Knowledge: Team software development, teamwork, agile practices, documentation of development elements (modeling, requirements specifications, evaluation, and product, test, user, and process documentation), Android Studio, Kotlin, Xml, Json, Git, Jira.",
+    link: "https://gitfront.io/r/AndreaHelmersen/MsD28eJ9hebs/Bewere/",
+    image: Bewere,
+    workedOn: "Spring 2022",
+  },
+  {
+    title: "ASP Interpreter",
+    projectText:
+      "The project involved creating an ASP interpreter in Java. The ASP programming language is a simplified version of Python. The project was divided into modules: scanner, parser, expression evaluation, and statement and function evaluation, which together built an interpreter for ASP. Projects were carried out in pairs of two.",
+    knowledgeText:
+      "Acquired Knowledge: Java, pair programming, collaboration, object-oriented programming in larger projects, use of modules in larger projects, debugging, interpreting and understanding code written by others.",
+    link: "https://gitfront.io/r/AndreaHelmersen/18ef300e5e098d17360269ff8f91dba72998652a/Asp-Interpreter/",
+    image: AspInterpreter,
+    workedOn: "Autumn 2021",
+  },
+];
 
-export const STATIC_PROJECT_DATA = [
+export const STATIC_PROJECT_DATA_bus = [
   {
     title: "Decon-X",
     projectText:
@@ -17,28 +41,6 @@ export const STATIC_PROJECT_DATA = [
     link: "https://gitfront.io/r/AndreaHelmersen/Eo62QB3V989J/Decon-X/",
     image: deconXImage,
     workedOn: "Spring 2022",
-  },
-  {
-    title: "Bewere",
-    projectText:
-      "The project involved creating an Android app in Android Studio that utilized information from several of the Meteorological Institute's APIs. A part of the project was to document the work and processes well in a report. The project was carried out in a team of six students.",
-    IdeaText:
-      "Idea: The app provides an overview of various everyday risk factors that may give cause for concern. The user selects a preferred location on the map in the app, and the hazards are then displayed.",
-    knowledgeText:
-      "Acquired Knowledge: Team software development, teamwork, agile practices, documentation of development elements (modeling, requirements specifications, evaluation, and product, test, user, and process documentation), Android Studio, Kotlin, Xml, Json, Git, Jira.",
-    link: "https://gitfront.io/r/AndreaHelmersen/MsD28eJ9hebs/Bewere/",
-    image: Bewere, 
-    workedOn: "Spring 2022",
-  },
-  {
-    title: "ASP Interpreter",
-    projectText:
-      "The project involved creating an ASP interpreter in Java. The ASP programming language is a simplified version of Python. The project was divided into modules: scanner, parser, expression evaluation, and statement and function evaluation, which together built an interpreter for ASP. Projects were carried out in pairs of two.",
-    knowledgeText:
-      "Acquired Knowledge: Java, pair programming, collaboration, object-oriented programming in larger projects, use of modules in larger projects, debugging, interpreting and understanding code written by others.",
-    link: "https://gitfront.io/r/AndreaHelmersen/18ef300e5e098d17360269ff8f91dba72998652a/Asp-Interpreter/",
-    image: AspInterpreter, 
-    workedOn: "Autumn 2021",
   },
   {
     title: "Digital Transformation of NG",
@@ -59,7 +61,7 @@ export const STATIC_PROJECT_DATA = [
     knowledgeText:
       "Acquired Knowledge: Analysis of microenvironments, assessment of competitive advantages ('Sweet Spot'), market segmentation, market research, marketing plan development, teamwork.",
     link: "gitfront.io/r/AndreaHelmersen/1c2a35fad9c12503c16a010c281db885c3387e9c/Prosjektoppgave-Tise/",
-    image: Tise, 
+    image: Tise,
     workedOn: "Spring 2021",
   },
 ];
