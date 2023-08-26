9582fbcfed4bc4e46c43ca25f2b3535e12879ff5
Andrea Arstad Helmersen <andrahe@ulrik.uio.no>
2023-08-26 20:18:05 +0200

    Fix in text in project data

--- src/staticData/ProjectData.tsx
+++ src/staticData/ProjectData.tsx
@@ -32,7 +32,7 @@ export const STATIC_PROJECT_DATA_tech = [
     IdeaText:
       "Idea: The app provides an overview of various everyday risk factors that may give cause for concern. The user selects a preferred location on the map in the app, and the hazards are then displayed.",
     knowledgeText:
-      "Acquired Knowledge: Team software development, teamwork, agile practices, documentation of development elements (modeling, requirements specifications, evaluation, and product, test, user, and process documentation), Android Studio, Kotlin, Xml, Json, Git, Jira.",
+      "Team software development, teamwork, agile practices, documentation of development elements (modeling, requirements specifications, evaluation, and product, test, user, and process documentation), Android Studio, Kotlin, Xml, Json, Git, Jira.",
     link: "https://gitfront.io/r/AndreaHelmersen/MsD28eJ9hebs/Bewere/",
     image: Bewere,
     workedOn: "Spring 2022",
@@ -42,7 +42,7 @@ export const STATIC_PROJECT_DATA_tech = [
     projectText:
       "The project involved creating an ASP interpreter in Java. The ASP programming language is a simplified version of Python. The project was divided into modules: scanner, parser, expression evaluation, and statement and function evaluation, which together built an interpreter for ASP. Projects were carried out in pairs of two.",
     knowledgeText:
-      "Acquired Knowledge: Java, pair programming, collaboration, object-oriented programming in larger projects, use of modules in larger projects, debugging, interpreting and understanding code written by others.",
+      "Java, pair programming, collaboration, object-oriented programming in larger projects, use of modules in larger projects, debugging, interpreting and understanding code written by others.",
     link: "https://gitfront.io/r/AndreaHelmersen/18ef300e5e098d17360269ff8f91dba72998652a/Asp-Interpreter/",
     image: AspInterpreter,
     workedOn: "Autumn 2021",
@@ -69,7 +69,7 @@ export const STATIC_PROJECT_DATA_bus = [
     companyText:
       "In this project we worked with Norsk gjenvinning (NG), translated to Norwegian Recycling",
     knowledgeText:
-      "Acquired Knowledge: Digital transformation, change management and resistance, assessment of organizational culture and environments, formulation of measures to achieve a desired future (measures for structure, leadership, and change strategy), teamwork.",
+      "Digital transformation, change management and resistance, assessment of organizational culture and environments, formulation of measures to achieve a desired future (measures for structure, leadership, and change strategy), teamwork.",
     link: "https://gitfront.io/r/AndreaHelmersen/d3d9f53f86a99d61ab5f529f4298894952e66a2b/Digital-transformasjon-av-NG/",
     image: NG,
     workedOn: "Autumn 2021",
@@ -79,7 +79,7 @@ export const STATIC_PROJECT_DATA_bus = [
     projectText:
       "In this project, we started with a product from a Norwegian high-tech company, mainly developing a plan for the product, distribution, pricing, and marketing communication for the selected target audience in 2022. The chosen product was Tise's sharing economy platform, primarily for the used clothing market.",
     knowledgeText:
-      "Acquired Knowledge: Analysis of microenvironments, assessment of competitive advantages ('Sweet Spot'), market segmentation, market research, marketing plan development, teamwork.",
+      "Analysis of microenvironments, assessment of competitive advantages ('Sweet Spot'), market segmentation, market research, marketing plan development, teamwork.",
     link: "gitfront.io/r/AndreaHelmersen/1c2a35fad9c12503c16a010c281db885c3387e9c/Prosjektoppgave-Tise/",
     image: Tise,
     workedOn: "Spring 2021",
